de1ce28ee96fe0f82191779bae9a8b65f0922615
Joe Kutner <jpkutner@gmail.com>
2014-08-06 10:17:45 -0500

    fixed formatting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,48 +8,48 @@ import java.net.URISyntaxException;
 import java.sql.*;
 
 public class Main extends HttpServlet {
-      @Override
-      protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-          throws ServletException, IOException {
-        try {
-          Connection connection = getConnection();
-          
-          Statement stmt = connection.createStatement();
-          stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
-          stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
-          stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-          ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-          
-          String out = "Hello!\n";
-          while (rs.next()) {
-              out += "Read from DB: " + rs.getTimestamp("tick") + "\n";
-          }
-          
-          resp.getWriter().print(out);
-        } catch (URISyntaxException e) {
-          resp.getWriter().print("There was an error: " + e.getMessage());
-        } catch (SQLException e) {
-          resp.getWriter().print("There was an error: " + e.getMessage());
-        }
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    try {
+      Connection connection = getConnection();
+      
+      Statement stmt = connection.createStatement();
+      stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
+      stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
+      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+      
+      String out = "Hello!\n";
+      while (rs.next()) {
+          out += "Read from DB: " + rs.getTimestamp("tick") + "\n";
       }
+      
+      resp.getWriter().print(out);
+    } catch (URISyntaxException e) {
+      resp.getWriter().print("There was an error: " + e.getMessage());
+    } catch (SQLException e) {
+      resp.getWriter().print("There was an error: " + e.getMessage());
+    }
+  }
 
-      private Connection getConnection() throws URISyntaxException, SQLException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+  private Connection getConnection() throws URISyntaxException, SQLException {
+    URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+    String username = dbUri.getUserInfo().split(":")[0];
+    String password = dbUri.getUserInfo().split(":")[1];
+    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
 
-        return DriverManager.getConnection(dbUrl, username, password);
-      }
+    return DriverManager.getConnection(dbUrl, username, password);
+  }
 
-      public static void main(String[] args) throws Exception{
-        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
-        server.setHandler(context);
-        context.addServlet(new ServletHolder(new Main()),"/*");
-        server.start();
-        server.join();
-      }
+  public static void main(String[] args) throws Exception{
+    Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+    context.setContextPath("/");
+    server.setHandler(context);
+    context.addServlet(new ServletHolder(new Main()),"/*");
+    server.start();
+    server.join();
+  }
 }
